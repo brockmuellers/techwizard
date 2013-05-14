566c777d2637d1c4f33593c3f2a5a327a2d99011
sarab609 <sarab609@mit.edu>
2013-05-14 16:32:33 -0400

    divided slides, disabled buttonz

--- js/slide_creator.js
+++ js/slide_creator.js
@@ -4,7 +4,9 @@ function createSlides() {
 	var firstSlide = $("#tut1")
 	var rest = $(".tutorial-slide").not("#tut1")
 	slide_count = 1 + rest.length
-	rest.hide()
+	rest.hide();
+	//disable previous button
+	document.getElementById("previousbtn").setAttribute("class", "btn disabled");
 }
 
 function advanceSlide() {
@@ -16,6 +18,9 @@ function advanceSlide() {
 	current_slide ++
 	var slide = "#tut" + current_slide
 	$(slide).show("slide", { direction: "right" }, 800);
+	//enable previous button and if at end, disable next button
+	document.getElementById("previousbtn").setAttribute("class", "btn");
+	document.getElementById("nextbtn").setAttribute("class", "btn disabled");
 }
 
 function previousSlide() {
@@ -29,6 +34,10 @@ function previousSlide() {
 	setTimeout(function(){
 	$(slide).show("slide", { direction: "left" }, 500);
 	},450);
+	//enable next button
+	//if at beginning, disable previous button
+	document.getElementById("nextbtn").setAttribute("class", "btn");
+	document.getElementById("previousbtn").setAttribute("class", "btn disabled");
 }
 
 $(document).ready(function() {
